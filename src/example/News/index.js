8136cacc701a855d2580a3b1d3bb751ea2968332
HarryChen0506 <chenwenhua0506@sina.com>
2018-04-09 21:35:33 +0800

    搭建框架

--- src/example/News/index.js
+++ src/example/News/index.js
@@ -1,13 +1,14 @@
 //新闻组件 News
 import React from 'react';
 import NewsBox from '../NewsBox';
-import ImagePreviewer from '../../lib/index.js'
-// import ImagePreviewer from '../../../dist/react-image-viewer-mobile.js'
+import { ImageViewer } from '../../lib/components'
+
+// import { ImageViewer } from '../../../dist/react-component-harry.js'
 class News extends React.Component {  
   render() {    
     return (      
         <div>
-          <ImagePreviewer 
+          <ImageViewer 
             async={true}             /*图片内容是否异步*/
             imageWrapperHeight={300}  /*图片容器高度*/
             opacity={1}          /*背景透明度*/
@@ -15,7 +16,7 @@ class News extends React.Component {
             gap={30}                 /*图片间隙*/
           >
             <NewsBox content={this.props.content}/>     
-          </ImagePreviewer>             
+          </ImageViewer>             
         </div>      
     );
   }
---
+++ src/lib/components/index.js
@@ -0,0 +1,7 @@
+//组件管理rukou 
+
+import ImageViewer from './ImageViewer';
+
+export {
+    ImageViewer
+}
